121ab67fb48f6692bcb1656f7de4c290cd44125d
sayurihaku <sayuri0411.chocolate@gmail.com>
2017-03-19 14:32:31 +0900

    playerが空を飛ぶ

---
+++ Assets/script/Drink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink : MonoBehaviour {
+	public float energy = 500;
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+	}
+
+	public float jet(float _energy){
+		
+		if (energy - _energy >= 0) {
+			energy -= _energy;
+			return _energy;
+
+		} else {
+			
+			return 0;
+		}
+	}
+}
