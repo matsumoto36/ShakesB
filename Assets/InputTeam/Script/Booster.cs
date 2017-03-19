a3a419ec65a4219af1641dc9b58e4ee3c408cac7
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-03-19 16:09:35 +0900

    コーラブースターの追加

---
+++ Assets/InputTeam/Script/Booster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour {
+
+	public bool isPlay;
+	bool _isPlay;
+
+	ParticleSystem p;
+
+	// Use this for initialization
+	void Start () {
+		isPlay = true;
+		_isPlay = true;
+		p = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(isPlay ^ _isPlay) {
+			_isPlay = isPlay;
+			EnableParticle(isPlay);
+		}
+
+	}
+
+	void EnableParticle(bool enable) {
+		if(enable) {
+			p.Play();
+		}
+		else {
+			p.Stop();
+		}
+
+	}
+}
